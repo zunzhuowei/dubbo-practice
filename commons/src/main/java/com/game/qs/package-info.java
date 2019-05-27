af4527086f9e98397aa62b561bf039ea271541e8
zunzhuowei <1808862723@qq.com>
2019-05-27 17:45:57 +0800

    user consumer、provider and commons modules init commit

---
+++ commons/src/main/java/com/game/qs/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Created by zun.wei on 2019/5/27 17:44.
+ * Description:
+ */
+package com.game.qs;
---
+++ user-provider/src/main/java/com/game/qs/UserProviderApp.java
@@ -0,0 +1,17 @@
+package com.game.qs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zun.wei on 2019/5/27 17:26.
+ * Description:
+ */
+@SpringBootApplication
+public class UserProviderApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserProviderApp.class, args);
+    }
+
+}
